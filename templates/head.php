81350fc1fde5520dfa4717a4397c5be01c587883
miukimiu <elizabet.oliveira@gmail.com>
2015-09-16 23:37:40 +0100

    Color fixes

--- templates/head.php
+++ templates/head.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href='https://fonts.googleapis.com/css?family=Lato:400,700,400italic' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
 </head>
